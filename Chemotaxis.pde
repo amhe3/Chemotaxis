955d75f05b3cf0d553d40e12677673ca59b0f8ad
unknown <amhe3@697-334-Theoden.sf.k12.internal>
2015-09-18 10:45:24 -0700

    experimented with add pop. (no progress)

--- Chemotaxis.pde
+++ Chemotaxis.pde
@@ -1,11 +1,12 @@
  //declare bacteria variables here  
-  int pop = 100;
+ int pop = 5;
  Bacteria [] colony = new Bacteria[pop]; //make "rooms"
 
  void setup()   
  {     
  	//initialize bacteria variables here   
  	size(400, 400);
+ 	frameRate(30);
  	for(int make = 0; make < colony.length; make++) //make bacteria
  	{
  		colony[make] = new Bacteria();
@@ -22,29 +23,32 @@
  		colony[num].show();
  	}
  	
- 	int addSub = (int)(Math.random()*10)
- 	if(addSub = 3)
+	/*int addSub = (int)(Math.random()*51);
+ 	if(addSub == 25)
  	{
  		pop++; //population increase
+ 		redraw();
  	}
- 	else if(addSub = 7) 
+ 	else if(addSub == 50) 
  	{
  		pop--; //population decrease
+ 		redraw();
  	}
 
  	fill(0);
  	textSize(20);
- 	text("Population: " + pop, 2, 21);
+ 	text("Population: " + pop, 2, 21);*/
  }  
 
  class Bacteria    
  {     
- 	int myX, myY, colors;
+ 	int myX, myY, colors, addSub;
  	Bacteria()
  	{
  		myX = 200;
  		myY = 200;
  		colors = color((int)(Math.random()*113)+112, (int)(Math.random()*113)+112, (int)(Math.random()*113)+112);
+ 		
  	}
  	void walk()
  	{
@@ -55,4 +59,5 @@
  	{
  		fill(colors);
  		ellipse(myX, myY, 10, 10);
+ 	}
  }    
--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -12,3 +12,87 @@ import java.io.InputStream;
 import java.io.OutputStream; 
 import java.io.IOException; 
 
+public class Chemotaxis extends PApplet {
+
+ //declare bacteria variables here  
+ int pop = 5;
+ Bacteria [] colony = new Bacteria[pop]; //make "rooms"
+
+ public void setup()   
+ {     
+ 	//initialize bacteria variables here   
+ 	size(400, 400);
+ 	frameRate(30);
+ 	for(int make = 0; make < colony.length; make++) //make bacteria
+ 	{
+ 		colony[make] = new Bacteria();
+ 	}
+ }   
+
+ public void draw()   
+ {    
+ 	//move and show the bacteria  
+ 	background(255);
+ 	for(int num = 0; num < colony.length; num++) //show
+ 	{
+ 		colony[num].walk();
+ 		colony[num].show();
+ 	}
+ 	
+	/*int addSub = (int)(Math.random()*51);
+ 	if(addSub == 25)
+ 	{
+ 		pop++; //population increase
+ 		redraw();
+ 	}
+ 	else if(addSub == 50) 
+ 	{
+ 		pop--; //population decrease
+ 		redraw();
+ 	}
+
+ 	fill(0);
+ 	textSize(20);
+ 	text("Population: " + pop, 2, 21);*/
+ }  
+
+ class Bacteria    
+ {     
+ 	int myX, myY, colors, addSub;
+ 	Bacteria()
+ 	{
+ 		myX = 200;
+ 		myY = 200;
+ 		colors = color((int)(Math.random()*113)+112, (int)(Math.random()*113)+112, (int)(Math.random()*113)+112);
+ 		
+ 	}
+ 	public void walk()
+ 	{
+ 		myX = myX + (int) (Math.random()*5)-2;
+ 		myY = myY + (int) (Math.random()*5)-2;
+ 		if(get(myX+10, myY+10) != color(255, 255, 255))
+ 		{
+ 			myX = myX + (int) (Math.random()*3)-2; //go left
+ 			myY = myY + (int) (Math.random()*3)-2; //go up
+ 		}
+ 		if(get(myX-10, myY-10) != color(255, 255, 255))
+ 		{
+ 			myX = myX + (int) (Math.random()*3); //go right
+ 			myY = myY + (int) (Math.random()*3); //go down
+ 		}
+ 	}
+ 	public void show()
+ 	{
+ 		fill(colors);
+ 		ellipse(myX, myY, 10, 10);
+ 	}
+ }    
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Chemotaxis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
